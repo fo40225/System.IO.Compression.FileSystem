0a49b194e50112adf88f0872ab84a9c52e8dc5d7
fo40225 <fo40225@hotmail.com>
2016-07-04 18:34:53 +0800

    add code, build pass.

---
+++ System.IO.Compression.FileSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: CLSCompliant(true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyCopyright("© Microsoft Corporation.  All rights reserved.")]
+[assembly: AssemblyDefaultAlias("System.IO.Compression.FileSystem.dll")]
+[assembly: AssemblyDelaySign(true)]
+[assembly: AssemblyDescription("System.IO.Compression.FileSystem.dll")]
+[assembly: AssemblyFileVersion("4.0.30319.18020")]
+[assembly: AssemblyInformationalVersion("4.0.30319.18020")]
+
+[assembly: AssemblyProduct("Microsoft® .NET Framework")]
+
+[assembly: AssemblyTitle("System.IO.Compression.FileSystem.dll")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: SatelliteContractVersion("4.0.0.0")]
+[assembly: CompilationRelaxations(8)]
+
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: ComVisible(false)]
+[assembly: SecurityRules(SecurityRuleSet.Level2, SkipVerificationInFullTrust = true)]
+[assembly: SecurityTransparent]
---
+++ System.IO.Compression/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: CLSCompliant(true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyCopyright("© Microsoft Corporation.  All rights reserved.")]
+[assembly: AssemblyDefaultAlias("System.IO.Compression.dll")]
+[assembly: AssemblyDelaySign(true)]
+[assembly: AssemblyDescription("System.IO.Compression.dll")]
+[assembly: AssemblyFileVersion("4.0.30319.18020")]
+[assembly: AssemblyInformationalVersion("4.0.30319.18020")]
+
+[assembly: AssemblyProduct("Microsoft® .NET Framework")]
+
+[assembly: AssemblyTitle("System.IO.Compression.dll")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: SatelliteContractVersion("4.0.0.0")]
+[assembly: CompilationRelaxations(8)]
+
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: ComVisible(false)]
+[assembly: SecurityRules(SecurityRuleSet.Level2, SkipVerificationInFullTrust = true)]
+[assembly: SecurityTransparent]
+
+[assembly: TypeForwardedTo(typeof(CompressionMode))]
+[assembly: TypeForwardedTo(typeof(DeflateStream))]
+[assembly: TypeForwardedTo(typeof(GZipStream))]
